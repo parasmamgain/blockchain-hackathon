3ed436605996fe03598e064126adb426ed755cb9
tanvibhakta <tanvibhakta@gmail.com>
2018-01-21 03:43:10 +0530

    finish root and dashboard route functions

--- first.py
+++ first.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 from flask import Flask
 from flask import *
-from frames.department import Department
+from frames.user import User
+from frames.request import Request
+
 
 app = Flask(__name__)
 
@@ -19,10 +21,34 @@ def index():
 	# return some.getData("1234") + str(some.getDataFrom())
 	return render_template('index.html')
 
-@app.route('/list_request')
-def list_request():
-    return render_template('list_requests.html')
+@app.route('/dashboard')
+def show_dashboard():
+	user1 = {
+		"id": "001",
+		"is_logged_in": False,
+		"requests": ["01", "02"]
+	}
+	req1 = {
+	"id": "01",
+	"timestamp":"t62u3ygq",
+	"status":"In Queue",
+	"category": "Felling"
+	}
+	req2 = {
+	"id": "02",
+	"timestamp":"t62u3sdygq",
+	"status":"In Process",
+	"category": "Transit"
+	}
+	user = User(user1)
+	user.is_logged_in = True
+	requests = user.requests
 
+	return render_template('dashboard.html', requests=requests)
+
+@app.route('/new-felling-request')
+def new_felling_request():
+	return render_template('new_felling_request.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
