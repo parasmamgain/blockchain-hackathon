a966c03c39109a5b6aac99c7fbb0d1601243b33b
Amarnath Bagineni <amarnath.bagineni@gmail.com>
2018-01-20 10:26:40 +0530

    Adding requirements.txt

---
+++ frames/rules-simulator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# Module for collection for the rules application simulation
+
+class Rules(object):
+	"""docstring for Rules"""
+	def __init__(self, arg):
+		super(Rules, self).__init__()
+		self.arg = arg
+		
+	RTO_approval = True
+	BBMP_approval = True
+
+	def is_RTO_approval():
+
+		# update the block chain with the status or the process
+		return self.RTO_approval
+
+	def is_BBMP_approval():
+
+		# update the block chain with the status or the process
+		return self.BBMP_approval
